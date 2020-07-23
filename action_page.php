df2578c2c601c2ef2cd27ecac14dea2cdf641082
AAxford <alan@axford.com>
2020-07-23 09:03:46 -0400

    first commit

---
+++ action_page.php
@@ -0,0 +1,8 @@
+<?php
+
+$fname = $_POST[fname];
+$lname = $_POST[lname];
+
+echo ("Your Full Name is: ".$fname." ".$lname);
+
+?>   
---
+++ skcoders/scripts/main.js
@@ -0,0 +1,5 @@
+/*
+Everything in between is a comment.
+*/
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world! Again";
